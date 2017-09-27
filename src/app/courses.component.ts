932c3ba6f8f6379e9fdcd958d7529eb3cc637b85
White, Durward <durward.white@enablemidstream.com>
2017-09-27 15:19:09 -0500

    working on trying to get the modal dialog to close

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -24,6 +24,8 @@ export class CoursesComponent implements OnInit {
     courses: Course[];
     course: Course;
     dotw: DaysOfTheWeek;
+    modalReference: any;
+
 
     constructor(private courseService: CourseService, private modalService: NgbModal) {}
     
@@ -36,14 +38,33 @@ export class CoursesComponent implements OnInit {
         // var tempCourse = {id: 1, name:'Spanish', duration:'50mins', startTime: '1:10p', days: 'Mo, We, Fr'};
         // this.courses.push(tempCourse);
         var dtw = days.monday;
-        
+        if (days.monday) {
+            newCourse.days = "Mo,";
+        }
+        if (days.tuesday) {
+            newCourse.days += "Tu,";
+        }
+        if (days.wednesday) {
+            newCourse.days += "We,";            
+        }
+        if (days.thursday) {
+            newCourse.days += "Th,";
+        }
+        if (days.friday) {
+            newCourse.days += "Fr,";
+        }
 
-        alert(days.monday);
+        console.log(newCourse.days.charAt(newCourse.days.length-1));
+        newCourse.days.replace(newCourse.days.charAt(newCourse.days.length-1), ""); 
+        this.courses.push(newCourse);
+        this.modalReference.close();
     }
 
     open(content: any) {
+        this.course = new Course();
+
         this.closeResult = '';
-        this.modalService.open(content).result.then((result) => {
+        this.modalReference = this.modalService.open(content).result.then((result) => {
           this.closeResult = `Closed with: ${result}`;
         }, (reason) => {
           this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
